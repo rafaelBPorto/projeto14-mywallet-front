0dcc71f57dfc7e4d426c9f6e463019ec65bb4577
rafaelBporto <rafaelbporto88@gmail.com>
2022-11-19 20:11:01 -0300

    feat: create GlobalStyle

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import GlobalStyle from "./assets/styles/GlobalStyle.js";
 import Home from "./pages/Home/Home.js";
 import Inflow from "./pages/Inflow/Inflow.js";
 import Outflow from "./pages/OutFlow/OutFlow.js";
@@ -7,15 +8,17 @@ import SingUp from "./pages/SingUp/SingUp.js";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<SingIn />} />
-        <Route path="/sing-up" element={<SingUp />} />
-        <Route path="/home" element={<Home />} />
-        <Route path="inflow" element={<Inflow />} />
-        <Route path="outflow" element={<Outflow />} />
-      </Routes>
-    </BrowserRouter>
+    <GlobalStyle >
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<SingIn />} />
+          <Route path="/sing-up" element={<SingUp />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="inflow" element={<Inflow />} />
+          <Route path="outflow" element={<Outflow />} />
+        </Routes>
+      </BrowserRouter>
+    </GlobalStyle>
   );
 }
 
