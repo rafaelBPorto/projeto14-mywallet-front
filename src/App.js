601d787749e27499de32475fc4aed88fa0e1f71c
rafaelBporto <rafaelbporto88@gmail.com>
2022-11-19 20:03:04 -0300

    feat: basic routes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home/Home.js";
+import Inflow from "./pages/Inflow/Inflow.js";
+import Outflow from "./pages/OutFlow/OutFlow.js";
 import SingIn from "./pages/SingIn/SingIn.js";
+import SingUp from "./pages/SingUp/SingUp.js";
 
 function App() {
   return (
     <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<SingIn />} />
-        </Routes>
-      </BrowserRouter>
+      <Routes>
+        <Route path="/" element={<SingIn />} />
+        <Route path="/sing-up" element={<SingUp />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="inflow" element={<Inflow />} />
+        <Route path="outflow" element={<Outflow />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/pages/Inflow/Inflow.js
@@ -0,0 +1,5 @@
+export default function Inflow(){
+    return(
+        <div>Inflow</div>
+    )
+}
