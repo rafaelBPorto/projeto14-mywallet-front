04bf86f12e02b03dc40148777ec61881f7565b0a
rafaelBporto <rafaelbporto88@gmail.com>
2022-11-20 15:21:47 -0300

    feat: Componet SingIn go to Home if exist token

--- src/pages/SingIn/SingIn.js
+++ src/pages/SingIn/SingIn.js
@@ -1,7 +1,19 @@
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 import { StyleScreen } from "../../assets/styles/StyleScreen";
 import SingInForm from "./components/SingInForm";
 
 export default function SingIn(){
+    const navigate = useNavigate()
+
+    useEffect(()=>{
+        if(localStorage?.getItem("token")){
+            navigate("/home")
+        }
+    },[navigate])
+
+
+
     return(
         <StyleScreen>
             <h1>MyWallet</h1>
