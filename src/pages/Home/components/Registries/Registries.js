e25f6a519866caeea11c704cba4490ea1e68b66c
rafaelBporto <rafaelbporto88@gmail.com>
2022-11-21 16:17:39 -0300

    feat: show registres and user

--- src/pages/Home/components/Registries/Registries.js
+++ src/pages/Home/components/Registries/Registries.js
@@ -1,9 +1,67 @@
-import StyleRegistries from "./StyleRegistries";
+import StyleRegistries from "./StyleRegistries.js";
+import axios from "axios";
+import { BASEURL } from "../../../../Constants/URLS"
+import { useEffect, useState } from "react";
+import dayjs from "dayjs";
+import styled from "styled-components";
 
-export default function Registries(){
-    return(
+export default function Registries() {
+    const [registries, setRegistries] = useState();
+
+    useEffect(() => {
+        async function getRegistries() {
+            const token = localStorage.getItem("token");
+            try {
+                const res = await axios.get(`${BASEURL}/registries`,
+                    {
+                        headers: { "authorization": `Bearer ${token}` }
+                    });
+                setRegistries(res.data.registries)
+            } catch (error) {
+                console.log(error)
+            }
+        }
+        getRegistries();
+    }, [])
+
+    if (registries === undefined) {
+        return <StyleRegistries><h2>Não há registros de entrada ou saída</h2></StyleRegistries>
+    }
+
+    function formatDate(date) {
+        return "recebidp"
+    }
+    return (
         <StyleRegistries>
-            Inicio
-        </StyleRegistries> 
+            {registries.map((r, index) =>
+                <Extract key={index}>
+                    <span> <StyleDate>{r.date}</StyleDate> {r.description} </span>
+                    <StyleType type={r.type==="outflow"? "#C70000" : "#03AC00"}>{Math.abs(r.value)}</StyleType>
+                </Extract>
+            )
+            }
+        </StyleRegistries >
     )
 }
+
+const Extract = styled.div`
+ display: flex;
+    justify-content: space-between;
+
+    
+    font-family: 'Raleway';
+    font-style: normal;
+    font-weight: 400;
+    font-size: 16px;
+    p{
+        
+    flex-direction:column;
+    }
+`
+const StyleDate = styled.span`
+    color:#C6C6C6;
+    margin-right: 10px;
+`
+const StyleType = styled.span`
+    color: ${props => props.type}
+`
